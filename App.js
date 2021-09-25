2ca470005942daeec4dbc95cc2720c0dc0269f95
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-09-25 22:07:27 +0900

    3.17 Drag and Drop Project part One

--- App.js
+++ App.js
@@ -4,24 +4,33 @@ import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import icons from "./icons";
 
+const BLACK_COLOR = "#1e272e";
+const GREY = "#485460";
+const GREEN = "#2ecc71";
+const RED = "#e74c3c";
+
 const Container = styled.View`
   flex: 1;
-  background-color: #34495e;
+  background-color: ${BLACK_COLOR};
 `;
-
 const Edge = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
+`;
+
 const Word = styled.Text`
-  font-weight: 600;
   font-size: 38px;
-  background-color: white;
-  padding: 10px 20px;
-  overflow: hidden;
-  border-radius: 15px;
-  color: ${(props) => props.textColor};
+  font-weight: 500;
+  color: ${(props) => props.color};
 `;
 const Center = styled.View`
   flex: 3;
@@ -30,30 +39,65 @@ const Center = styled.View`
 `;
 const IconCard = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
-  padding: 10px;
-  border-radius: 5px;
+  padding: 10px 20px;
+  border-radius: 10px;
 `;
 
 export default function App() {
   // Values
+  const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
   // Animations
-
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goHome = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
   // Pan Responders
-
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderRelease: () => {
+        Animated.parallel([onPressOut, goHome]).start();
+      },
+    })
+  ).current;
   // State
+
   return (
     <Container>
       <Edge>
-        <Word textColor={"#2ecc71"}>알아</Word>
+        <WordContainer>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
       </Edge>
       <Center>
-        <IconCard style={{ transform: position.getTranslateTransform() }}>
-          <Ionicons name="pizza" color="#34495e" size={68} />
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            transform: [...position.getTranslateTransform(), { scale }],
+          }}
+        >
+          <Ionicons name="beer" color={GREY} size={76} />
         </IconCard>
       </Center>
       <Edge>
-        <Word textColor={"#e74c3c"}>몰아</Word>
+        <WordContainer>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
       </Edge>
     </Container>
   );
